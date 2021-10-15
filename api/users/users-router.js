696a47c49ea711ae9b083d359cf0b2d7a392d508
Pauline122112 <pauline.design007@gmail.com>
2021-10-15 14:06:23 -0500

    created user-router.js

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,2 +1,22 @@
 const express = require("express")
 const router = express.Router()
+const Users = require("./usersModel.js");
+
+router.get('/' (req, res, next) => {
+  Users.find()
+    .then((users) => {
+      res.json(users)
+    })
+    .catch(next)
+})
+
+router.get('/:user_id' (req, res, next) => {
+    Users.findById(req.params.user_id)
+    .then((user) => {
+        res.json(user)
+    })
+    .catch(next)
+})
+
+
+module.exports = router
