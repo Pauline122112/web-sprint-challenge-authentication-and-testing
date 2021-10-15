b02c2aae9323cc524f816b0f989be39f9e597e82
Pauline122112 <pauline.design007@gmail.com>
2021-10-15 11:34:23 -0500

    made some changes

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,25 +1,25 @@
-const db = require("../../data/dbConfig");
+const db = require("../../data/dbConfig")
 
 function getAll() {
-	return db("users");
+	return db("users")
 }
 
 function getById(id) {
-	return db("users").where({ id }).first();
+	return db("users").where({ id }).first()
 }
 
 async function insert(user) {
-	const [id] = await db("users").insert(user);
+	const [id] = await db("users").insert(user)
 	return getById(id);
 }
 
 async function update(id, changes) {
-	await db("users").where({ id }).update(changes);
+	await db("users").where({ id }).update(changes)
 	return getById(id);
 }
 
 function remove(id) {
-	return db("users").where({ id }).del();
+	return db("users").where({ id }).del()
 }
 
 module.exports = {
